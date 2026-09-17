07ecbafdbc281a9fabe20c99858980b17113e25a
MHuzaifa-Dev29 <mmhuzaifa624@gmail.com>
2026-09-17 22:44:06 +0500

    git is completw

--- src/main.java
+++ src/main.java
@@ -1,5 +1,7 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("I am learning Git");
+        System.out.println("Learning Git and GitHub");
+        System.out.println("Git Done");
     }
 }
